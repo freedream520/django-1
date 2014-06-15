c8345b78ad5f52014f7ffa88da5e5896314a61ff
Christopher W. Heyer <cwheyer@umich.edu>
2014-06-14 20:06:37 -0500

    Setting up admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from polls.models import Poll, Choice
 
 # Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question', {'fields':['question']}),
+        ('Publication Date', {'fields':['pub_date']})]
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+
+admin.site.register(Poll, PollAdmin)
